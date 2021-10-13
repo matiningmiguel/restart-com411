181ceb4d1bdb456c78ead37be2ef0caf9c6dd1a9
unknown <5matim19@solent.ac.uk>
2021-10-13 22:08:44 +0100

    Added code to demonstrate the use of a modulus operator.

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,7 @@
+# Beep is now able to work out complex problems and puzzles!
+print("Please enter a whole number")
+num = int(input())
+if (num % 2 == 0):
+    print(f"The number {num} is an even number")
+else:
+    print(f"The number {num} is an odd number")
