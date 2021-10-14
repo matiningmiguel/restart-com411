c487433d03b87cdfa38e59c25c2f35f831e16b12
Miguel Matining <5matim19@solent.ac.uk>
2021-10-14 10:14:19 +0100

    Added code to demonsrate the use of a logical and operator.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,10 @@
+# Beep is deep into the forest, should he continue or not?
+print("What did I hear?")
+hear = input()
+print("What did I see?")
+see = input()
+if (hear == "grrr") and (see == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+
+else:
+    print("I am a little scared but I will continue")
